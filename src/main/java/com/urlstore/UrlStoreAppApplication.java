33a09b9bf8686bc0cd375a629d4f7cd0f4eb0d6d
RamRikhi <rikhi.bhanas.ram@gmail.com>
2021-07-30 11:29:30 +0530

    Initial commit

---
+++ src/main/java/com/urlstore/UrlStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.urlstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlStoreAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/urlstore/UrlStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
